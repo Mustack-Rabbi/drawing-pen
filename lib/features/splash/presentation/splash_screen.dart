4658aef13a7ba24fa3bfefb4def940c878e99f4e
Mustack-Rabbi <mustackrabbi@gmail.com>
2025-03-02 15:30:58 +0600

    Code Explain initState(){}, AnimationController, vsync - this;  splash_screen.dart

--- lib/features/splash/presentation/splash_screen.dart
+++ lib/features/splash/presentation/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// [A], [B], [C], [D];
+// [A], [B], [C], [D], [E];
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,8 +12,9 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   // [A]
-  late AnimationController _controller;
-  late Animation<double> _fadeAmination;
+  late AnimationController
+      _controller; //ekhane dekliyar kora hoyeche ebong pore inisaliz hbe.
+  late Animation<double> _fadeAmination; // ঐ
 
   /*
   [A] এখানে with ব্যাবহার করছি Mixin এর জন্যে। Mixin এর কাজ 
@@ -22,33 +23,54 @@ class _SplashScreenState extends State<SplashScreen>
 
   @override // [B]
   void initState() {
+    super.initState();
     //[C]
     _controller = AnimationController(
       // [D]
-      vsync: this,
+      vsync: this, // [E] //TickerProvider eti required ache.
       duration: const Duration(seconds: 2),
     );
-    _fadeAmination = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
-    _controller.forward();
+    _fadeAmination = Tween<double>(begin: 0.0, end: 1.0).animate(
+        _controller); // Tween duiti man dhape dhape poriborton kore. eti Animation controller er maddhome control korte hbe, eti nije nije pori borton hoy na.
+    _controller
+        .forward(); // Animation suru korte, thachara repeat().. bebohar korte parai #Comment: na dile amimation suru hbe na.
 
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacementNamed(context, "/home");
     });
-
-    super.initState();
   }
 
   /*
-  [B] @override ekhane prant class er methord override koreche ebong namer vul thakle error dekhay.  #BAD PRATICE(@override na likleo somossa hoy na)
-   Benefit: @override likhle methord name vul thakle error dekhay. @override na likhle amar mote methord er name vul korle override nao hte pare.
+  [B] @override এখানে prant class এর methord override করছে এবং এটি নামের ভুল 
+  থাকলে error দেখায়।  #BAD PRATICE: (@override না লিখলেও সমস্যা হয় না।) যদি নামের
+   ভুল না থাকে।
+   Benefit: @override লিখলে methord এর নাম ভুল থকলে error দেখায়।. 
+   #Comment:  (@override না লিখলে আমরা মতে methord এর নাম ভুল করলে override 
+   নাও হতে পারে।)
   
-  [C] initState(){} - prothome ekbar run hoy, jokhon ekbar State poriborton hoy. *State holo temporary data ja mutable & UI te poriborton ante pare.
+  [C] initState(){} - prothome ekbar run hoy, jokhon ekbar State poriborton 
+  hoy. *State holo temporary data ja mutable & UI te poriborton ante pare.
    
-  [D] AnimationController bebohar korchi animation korar jonne. 
-  Q. Keno "AnimationController ()" likhte hbe? ei namti evabei ba keno likhte hbe?
-  A. AnimationController() holo (import 'package:flutter/animation.dart';) ekhan theke eseche.
-  Q. ekhane to (import 'package:flutter/animation.dart';) package import kora hoyni tahole kaj korche kivabe?
-  A. (import 'package:flutter/material.dart';) er moddhe (import 'package:flutter/animation.dart';) package royeche..
+  [D] AnimationController এখানে ব্যাবহার করছি animation করার জন্যে। 
+  Question & Answer:
+  Q. Keno "AnimationController ()" likhte hbe? ei namti evabei ba keno likhte 
+  hbe?
+    A. AnimationController() holo (import 'package:flutter/animation.dart';) 
+    ekhan theke eseche. ei package e evabei name dewa ache.
+  Q. ekhane to (import 'package:flutter/animation.dart';) package import kora 
+  hoyni tahole kaj korche kivabe?
+    A. (import 'package:flutter/material.dart';) er moddhe
+     (import 'package:flutter/animation.dart';) package royeche..
+
+  [E] vsync - holo "vartical syncronization". vsync bebohar korle frames lek
+   kore na. vsync na bebohar korle flutter bujteparbe na kokhon update korte 
+   hbe, sob somoy run hbe bettary ebong CPU er bebohar besi hbe.
+
+   Question & Answer:
+   Q. vsync: this,  ekhne "this" keno?
+   A. "this" holo bortoman class er reference.
+   Q. reference ki?
+   A. eti kothay ache seti bole dewa.
    */
 
   @override
