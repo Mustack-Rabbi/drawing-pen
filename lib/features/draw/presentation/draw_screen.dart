411e956757a8ba36b6434333a6d9937b3e60f1da
Mustack-Rabbi <mustackrabbi@gmail.com>
2025-03-03 23:36:05 +0600

    draw page..

--- lib/features/draw/presentation/draw_screen.dart
+++ lib/features/draw/presentation/draw_screen.dart
@@ -1,10 +1,27 @@
+import 'package:drawing_pen/features/draw/models/stroke.dart';
 import 'package:flutter/material.dart';
 
-class DrawScreen extends StatelessWidget {
+class DrawScreen extends StatefulWidget {
   const DrawScreen({super.key});
 
   @override
+  State<DrawScreen> createState() => _DrawScreenState();
+}
+
+class _DrawScreenState extends State<DrawScreen> {
+  List<Stroke> strokes = [];
+  List<Stroke> _redoStokes = [];
+  List<Stroke> _currentPoints = [];
+  Color _selectedColor = Colors.black;
+  double _brushSize = 4.0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Draw Now"),
+      ),
+      body: Column(),
+    );
   }
 }
