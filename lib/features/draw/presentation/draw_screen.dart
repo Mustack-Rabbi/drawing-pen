8cf64f1860554490ffdb080f82125d3d9c8e9b30
Mustack-Rabbi <mustackrabbi@gmail.com>
2025-03-04 19:26:49 +0600

    draw draw_screen.dart

--- lib/features/draw/presentation/draw_screen.dart
+++ lib/features/draw/presentation/draw_screen.dart
@@ -1,5 +1,6 @@
 import 'package:drawing_pen/features/draw/models/stroke.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class DrawScreen extends StatefulWidget {
   const DrawScreen({super.key});
@@ -9,9 +10,9 @@ class DrawScreen extends StatefulWidget {
 }
 
 class _DrawScreenState extends State<DrawScreen> {
-  List<Stroke> strokes = [];
-  List<Stroke> _redoStokes = [];
-  List<Stroke> _currentPoints = [];
+  List<Stroke> _strokes = [];
+  List<Stroke> _redoStrokes = [];
+  List<Offset> _currentPoints = [];
   Color _selectedColor = Colors.black;
   double _brushSize = 4.0;
 
@@ -21,7 +22,92 @@ class _DrawScreenState extends State<DrawScreen> {
       appBar: AppBar(
         title: const Text("Draw Now"),
       ),
-      body: Column(),
+      body: Column(
+        children: [
+          Expanded(
+            child: GestureDetector(
+              onPanStart: (details) {
+                setState(() {
+                  _currentPoints.add(details.localPosition);
+                });
+              },
+              onPanUpdate: (details) {
+                setState(() {
+                  _currentPoints.add(details.localPosition);
+                });
+              },
+              onPanEnd: (details) {
+                setState(() {
+                  _strokes.add(Stroke(
+                      points: List.from(_currentPoints),
+                      color: _selectedColor,
+                      brushSize: _brushSize));
+                });
+                _currentPoints = [];
+                _redoStrokes = [];
+              },
+              child: CustomPaint(
+                painter: DrawPainter(
+                    strokes: _strokes,
+                    currentPaints: _currentPoints,
+                    currentColor: _selectedColor,
+                    currentBrushSize: _brushSize),
+                size: Size.infinite,
+              ),
+            ),
+          )
+        ],
+      ),
     );
+  }
+}
+
+class DrawPainter extends CustomPainter {
+  final List<Stroke> strokes;
+  final List<Offset> currentPaints;
+  final Color currentColor;
+  final double currentBrushSize;
+
+  DrawPainter({
+    super.repaint,
+    required this.strokes,
+    required this.currentPaints,
+    required this.currentColor,
+    required this.currentBrushSize,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    //Draw completed dtrokes
+
+    for (final stroke in strokes) {
+      final paint = Paint()
+        ..color = stroke.color
+        ..strokeCap = StrokeCap.round
+        ..strokeWidth = stroke.brushSize;
+
+      for (int i = 0; i < stroke.points.length; i++) {
+        if (stroke.points[i] != Offset.zero &&
+            stroke.points[i + 1] != Offset.zero) {
+          canvas.drawLine(stroke.points[i], stroke.points[i + 1], paint);
+        }
+      }
+    }
+    final paint = Paint()
+      ..color = currentColor
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = currentBrushSize;
+
+    for (int i = 0; i < currentPaints.length; i++) {
+      if (currentPaints[i] != Offset.zero &&
+          currentPaints[i + 1] != Offset.zero) {
+        canvas.drawLine(currentPaints[i], currentPaints[i + 1], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
